a94f68bf9bcb034fa994a5bb5931a1c94e85e329
Ash Furrow <ash@ashfurrow.com>
2013-07-31 16:33:57 -0400

    Initial Commit

---
+++ UICollectionView-Spring-Demo/TLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TLAppDelegate.h
+//  UICollectionView-Spring-Demo
+//
+//  Created by Ash Furrow on 2013-07-31.
+//  Copyright (c) 2013 Teehan+Lax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UICollectionView-Spring-Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UICollectionView-Spring-Demo
+//
+//  Created by Ash Furrow on 2013-07-31.
+//  Copyright (c) 2013 Teehan+Lax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TLAppDelegate class]));
+    }
+}
